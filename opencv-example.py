a13a58f709fe393ef25d41302b25beb8ce92ef17
shreya0406 <96071006+shreya0406@users.noreply.github.com>
2023-03-13 17:18:25 +0530

    Update opencv-example.py

--- opencv-example.py
+++ opencv-example.py
@@ -1,7 +1,7 @@
 import cv2
 import imutils
 
-
+#read the image
 #image = cv2.imread('input_image.JPG')
 cap = cv2.VideoCapture('test_video.mp4')
 
@@ -11,10 +11,14 @@ while True:
 
     text = "This is my custom text"
     cv2.putText(frame, text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,0,255), 1)
+    #(image , pass text, pass the origin(X, Y coordinates),font,size of text,RGB, Thickness)
 
     cv2.rectangle(frame, (50,50), (500 , 500), (0, 0, 255), 2)
-
+    #(image, co-ordinates of 1st point, point 2 co-ordinates, RGB,Thickness)
     cv2.imshow('img', frame)
+    
+    #to save the image use imwrite()
+    #cv2.imwrite('output_image.jpg', image)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
